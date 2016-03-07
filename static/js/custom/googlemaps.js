b847dc33cfaa322136d552416956ea5ca8c009c2
Phohawkenics <leolanuzo@gmail.com>
2016-03-06 23:48:22 -0500

    Added js libs

---
+++ static/js/custom/googlemaps.js
@@ -0,0 +1,7 @@
+// Geolocation with HTML 5 and Google Maps API based on example from
+// maxheapsize: http://maxheapsize.com/2009/04/11/getting-the-browsers-geolocation-with-html-5/
+// This script is based in part on one from http://merged.ca/
+$(document).ready(function() {
+	
+	
+});
